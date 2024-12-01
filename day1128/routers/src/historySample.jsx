33b5c68702eb7cdbcba9631377cd67ba39e4ec5a
galaxy_star <kyejin0303@gmail.com>
2024-12-01 21:25:26 +0900

    1201

---
+++ day1128/routers/src/historySample.jsx
@@ -0,0 +1,28 @@
+import React,{useEffect} from "react";
+
+function HistorySample({history}){
+    const goBack = () =>{
+        history.goBack();//이전의 컴포넌트로 이동
+    }
+    const goHome = () =>{
+        history.push('/');//첫번째 컴포넌트로 이동
+    }
+
+    useEffect(() => { //이탈방지
+        console.log(history);
+        const unblock = history.block('정말 떠나실건가요?');
+        return () => {
+            unblock();
+        };
+    }, [history]);
+
+    return(
+        <>
+        <h1>History</h1>
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goHome}>홈으로</button>
+        </>
+    )
+}
+
+export default HistorySample;
--- day1128/routers/src/profiles.jsx
+++ day1128/routers/src/profiles.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import {Link,Route} from "react-router-dom";
-
+import Profile from './profile';
 
 const Profiles = () => {
     return(
         <div>
             <h1>유저목록</h1>
+            {/* 파라미터 : /profile/apple  */}
             <ul>
                 <li>
                     <Link to="/profiles/apple">apple</Link>
@@ -13,7 +14,14 @@ const Profiles = () => {
                 <li>
                     <Link to="/profiles/banana">banana</Link>
                 </li>
+                <li>
+                    <Link to="/profiles/mango">mango</Link>
+                </li>
             </ul>
+        <Route path="/profiles" exact render={() => <div>유저를 선택하세요</div>}/>
+        <Route path="/profiles/:username" component={Profile}/>
+
+
         </div>
     );
 };
